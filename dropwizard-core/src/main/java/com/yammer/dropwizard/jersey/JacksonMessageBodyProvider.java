8d79adda835eb610d19fff6dac569a1bc2023d1a
Bryan Pham <bryan@cloudtenna.com>
2013-03-30 20:05:45 -0700

    update to latest.

--- dropwizard-core/src/main/java/com/yammer/dropwizard/jersey/JacksonMessageBodyProvider.java
+++ dropwizard-core/src/main/java/com/yammer/dropwizard/jersey/JacksonMessageBodyProvider.java
@@ -20,9 +20,9 @@ import java.lang.reflect.Type;
 
 /**
  * A Jersey provider which enables using Jackson to parse request entities into objects and generate
- * response entities from objects. Any request entity method parameters annotated with {@code
+ * response entities from objects. Any request entity method parameters annotated with {@code}
  *
- * @Valid} are validated, and an informative 422 Unprocessable Entity response is returned should
+ * {@Valid} are validated, and an informative 422 Unprocessable Entity response is returned should
  * the entity be invalid.
  * <p/>
  * (Essentially, extends {@link JacksonJaxbJsonProvider} with validation and support for {@link
